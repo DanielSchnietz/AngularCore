957c977396c88168c644513b9c9e733d3afb6b7c
daniel <danielschnietz@web.de>
2020-11-12 15:41:42 +0100

    new overhead class

---
+++ Model/Overhead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebApp.Model
+{
+    public class Overhead
+    {
+        public string OverheadName { get; set; }
+        public int OverheadPercentage { get; set; }
+        public int OverheadValue { get; set; }
+        
+    }
+}
